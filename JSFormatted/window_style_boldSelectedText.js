d4e44681175136038499274d85d73537bdb58dd8
jhauga <johnhaugabook@gmail.com>
2024-11-24 16:04:27 -0500

    update: improve compatibility

--- JSFormatted/window_style_boldSelectedText.js
+++ JSFormatted/window_style_boldSelectedText.js
@@ -1,99 +1,137 @@
 javascript:(function() {
  /* OPTIONAL - set to 0 or delete to turn off instruciton pop-up, */
- var alertWithInstructions_boldText = 1;
+ var alertWithInstructions_boldText = 1;  /* turn instruction alert on or off                 */
+ var noPressKey_boldText = "SHIFT";       /* button to press when using with other bookmarkel */
 
- /* Define global variables.                    */ 
- var currentBoldCount = 1; /* count number of times run to get unique ids */
- var currentStyleOpenTag_boldText  = "<strong>";  /* open tag to style selection  */
- var currentStyleCloseTag_boldText = "</strong>"; /* close tag to style selection */
+ /* Define global variables. */ 
+ var numberOfKeyPresses_boldText = 2;/* specify how many keypresses are used         */
+ var currentStyleOpenTag_boldText  = "<strong>";  /* open tag to style selection     */
+ var currentStyleCloseTag_boldText = "</strong>"; /* close tag to style selection    */ 
+ var keyCombo_boldText = "Control+b"; /* key combo to change style        */ 
  
  /* Declare global variables */
  var range_boldText, selection_boldText, selectionParent_boldText, 
      selectedString_boldText, parentIDWithSelectedText_boldText;
  
- /***** MAIN FUNCTION *****/
- function keypressToBookmarklet_boldText() {
-  /* keyboard combo that enablse text to be style bold */
-  let lastBoldKeypress = sessionStorage.getItem("bm_last_bold_keypress");
-  let currentBoldKeyPress = event.key;
-
-  /* check if sessionStorage already contains data with above var names */
-  if (lastBoldKeypress == null) {
-   sessionStorage.setItem("bm_last_bold_keypress", event.key);
-   lastBoldKeypress = sessionStorage.getItem("bm_last_bold_keypress");
+ /******************************* MAIN FUNCTION *******************************/
+ /*****************************************************************************/
+ var keySwitch_boldText = 0;      
+ var lastThreeKeys_boldText = []; /* enables last three key presses to be stored */
+ function keypressToBookmarklet_boldText(key_press) {
+  /* keyboard combo that enablse text to be style bold */       
+  lastThreeKeys_boldText.push(key_press);
+  
+  /* keep only the last three keys */
+  if (lastThreeKeys_boldText.length > numberOfKeyPresses_boldText) {
+   lastThreeKeys_boldText.shift();
   }
-  /* variable to check key combo */
-  let checkKeyComboBoldText = 
-   lastBoldKeypress + "+" + currentBoldKeyPress;
-
-  /* check if key combo is ctrl + b */
-  if (checkKeyComboBoldText == "Control+b") {   
-   currentBoldCount += 1;         /* increment count for unique id   */
-   getParentElementOfSelection(); /* get parent of selected text     */
-   
-   /* add a delay to ensure two previous blocks run */ 
-   setTimeout(function() { 
-    boldTheSelectedText(); /* wrap the selected text in <strong> tag */
-   }, 100);
+  
+  /* check key combo                                   */
+  if (numberOfKeyPresses_boldText == 2) {      
+   /* check if key combo is keyCombo_boldText variable */
+   let checkIfKeyCombo_boldText = 
+    lastThreeKeys_boldText[0] + "+" +
+    lastThreeKeys_boldText[1];
+   if (checkIfKeyCombo_boldText == keyCombo_boldText) {   
+    getParentElementOfSelection_boldText(); /* get parent of selected text     */
+   }
+  } 
+  else if (numberOfKeyPresses_boldText == 3) {
+   /* check if key combo is keyCombo_boldText variable */
+   let checkIfKeyCombo_boldText = 
+    lastThreeKeys_boldText[0] + "+" +
+    lastThreeKeys_boldText[1] + "+" +
+    lastThreeKeys_boldText[2];   
+   if (checkIfKeyCombo_boldText == keyCombo_boldText) {
+    getParentElementOfSelection_boldText(); /* get parent of selected text     */
+   }    
+  }
+  else {
+   /* check if key combo is keyCombo_boldText variable */
+   let checkIfKeyCombo_boldText = 
+    lastThreeKeys_boldText[0];
+   if (checkIfKeyCombo_boldText == keyCombo_boldText) {   
+    getParentElementOfSelection_boldText(); /* get parent of selected text     */
+   }
   }
  }
 
 
  /***** Support Functions: *****/ 
  /* Get parent of selection, give it id and use it to style selected text. */
- const getParentElementOfSelection = () => {
+ const getParentElementOfSelection_boldText = () => {
    /* get the current selection */
    selection_boldText = window.getSelection();
    
-   if (selection_boldText.rangeCount > 0) { /* ensure something is selected            */
-    /* prepare elements to change selection                                  */
-    range_boldText = selection_boldText.getRangeAt(0);  /* first object of selection       */
+   if (selection_boldText.rangeCount > 0) { /* ensure something is selected                 */
+    /* prepare elements to change selection                                                 */
+    range_boldText = selection_boldText.getRangeAt(0);  /* first object of selection        */
     selectionParent_boldText = range_boldText.commonAncestorContainer; /* node of selection */
-
-    selectedString_boldText = range_boldText.toString(); /* store as string      */     
-    /* ensure selection is a text node                                  */
-    if (selectionParent_boldText.nodeType === 3) { /* if it is a text node        */
-     let curPar = selectionParent_boldText.parentElement; /* store the parent element  */
-
-     /* give parent a unique id */
-     if (curPar.hasAttribute("id") == true) {
-      let curID = curPar.id;
-      if (curID.indexOf("bm_window_style_boldSelectedText_") > -1) {       
-       /* don't include prior unique ids */
-       curID = curID.replace(/bm_window_style_boldSelectedText_[0-9]+/g, "bm_window_style_boldSelectedText_" + currentBoldCount);
-       /* set the parents' id to unique id */
-       curPar.id = curID;
-      } else {
-       /* append unique id to the current id value */
-       curPar.id += " bm_window_style_boldSelectedText_" + currentBoldCount;
-      }
+ 
+    let temp = document.getElementById("bm_window_style");
+    if (temp) {
+     /* don't include prior global style ids */       
+     let tempID = temp.getAttribute("id");
+     if (tempID == "bm_window_style") {
+      temp.removeAttribute("id");
      } else {
-      /* set the parents' id to unique id */
-      curPar.setAttribute("id", "bm_window_style_boldSelectedText_" + currentBoldCount);
-     }       
-
-     /* select by id toget parentelement */
-     parentIDWithSelectedText_boldText = 
-      document.getElementById("bm_window_style_boldSelectedText_" + currentBoldCount);
-    } else {
-     let skip;
+      temp.setAttribute("id", tempID.replace("bm_window_style", ""));
+     }     
     }
-   }
+    
+     selectedString_boldText = range_boldText.toString(); /* store as string            */     
+     /* ensure selection is a text node                                                 */
+     if (selectionParent_boldText.nodeType === 3) { /* if it is a text node             */
+      let curPar = selectionParent_boldText.parentElement; /* store the parent element  */
+      let checkIfStyleApplied = /* check if the style has already been applied          */
+       currentStyleOpenTag_boldText.replace(/[<>]/g, "").toUpperCase();
+      
+      if (checkIfStyleApplied == curPar.tagName) { /* remove if already applied          */
+        let grandParentOfSelection = curPar.parentElement; /* get tag nesting selection  */
+        let curInnerHTML = grandParentOfSelection.innerText; /* innerText to replace     */
+        curInnerHTML = /* replace innderText, but remove style tag already applied       */
+         curInnerHTML.replace(/currentStyleOpenTag_boldText/, "").replace(/currentStyleCloseTag_boldText/, "");
+        /* update with the styled tag removed */
+        grandParentOfSelection.innerHTML = curInnerHTML;        
+      } else {
+      /* give parent a global style id                    */
+      if (curPar.hasAttribute("id") == true) {
+       let curID = curPar.id;
+       if (curID.indexOf("bm_window_style") > -1) {              
+        /* set the parents' id to global style id         */
+        curPar.id = "bm_window_style";
+       } else {
+        /* append global style id to the current id value */
+        curPar.id += " bm_window_style";
+       }
+      } else {
+       /* set the parents' id to global style id */
+       curPar.setAttribute("id", "bm_window_style");
+      }       
+    
+      /* select by id to get parent element */
+      parentIDWithSelectedText_boldText = 
+       document.getElementById("bm_window_style");
+      /* apply styling to selection */  
+      boldTheSelectedText();
+    }
+   } 
+  }
  };
  
- /* Style text using the ID added to parent element in getParentElementOfSelection(). */
+ /* Style text using the ID added to parent element in getParentElementOfSelection_boldText(). */
  const boldTheSelectedText = () => {
   /* store the curren innerHTML to update with bold text */
   let curInnerHTML = parentIDWithSelectedText_boldText.innerHTML;
 
-  /* wrap selection in HTML strong tag                                          */
-  let boldStyleSelection = /* use variables defined at top and in main function */
+  /* wrap selection in HTML u tag                                          */
+  let boldStyleSelection = /* use variables defined at top and in main function    */
    currentStyleOpenTag_boldText + selectedString_boldText + currentStyleCloseTag_boldText;
   
   /* if only one occurence of selected text in parent element */
   if (curInnerHTML.indexOf(selectedString_boldText) == 
       curInnerHTML.lastIndexOf(selectedString_boldText)) {
-   /* replace selection with newly styled text */
+   /* replace selection with newly styled text  */
    curInnerHTML = curInnerHTML.replace(selectedString_boldText, boldStyleSelection);
    
    /* Update parent HTML with newly styled text */
@@ -118,30 +156,41 @@ javascript:(function() {
    parentIDWithSelectedText_boldText.innerHTML = 
     insertAtIndex(curInnerHTML, boldStyleSelection, curIndex, curLengthOfText);
   }
+  selection_boldText = undefined;
+  lastThreeKeys_boldText = [];
  };
 
  /* Listen form key combo to bold text. */
  const addKeyDown_boldText = () => { 
-  document.body.addEventListener("keydown", function() {
-   keypressToBookmarklet_boldText();  
-  }); 
+  document.body.addEventListener("keydown", function(event) {
+   keypressToBookmarklet_boldText(event.key);  
+  });   
  }; 
  
  /****************************************************************** 
    Listen for keydonw event, running main function and bold selected
-   text if the keyboard input is "Ctrl + b".
+   text if the keyboard input is keyCombo_boldText variable.
  *******************************************************************/ 
  addKeyDown_boldText();
  
  /* Add alert event so that instructions for use are communicated. */
  if (alertWithInstructions_boldText != undefined && 
      alertWithInstructions_boldText == 1) {
-  let alertText = "Press 'Ctrl + b' to make selected text bold. \n\n" +
+  let alertText = 
+        "INSTRUCTIONS:     \n"   + 
+        "*************     \n"   +
+        "Press '" + keyCombo_boldText + "' to make selected text bold.             \n" +
+        "To remove press '" + keyCombo_boldText + "' some of previous styled text. \n\n" +
         "For best results: \n\n" +
-        " - Avoid selecting text with different sytling. \n"       +
+        " - Avoid selecting text with different sytling.   \n"     +
         " - Avoid selecting a word that is often repeated. \n"     +
-        " - Select text on the same line. \n"                      + 
-        " - Ensure selection contains entire words. \n";   
+        " - Press each key specified one at a time.        \n"     +
+        " - If using with another style bookmarklet, press '"      + noPressKey_boldText + "'\n" +
+        "   button THREE times with NO text selected       \n"     +
+        "   before styling another with a different style  \n"     +
+        "   and key combo.                                 \n"     +
+        " - If style is not applied, press just '" + 
+        keyCombo_boldText[keyCombo_boldText.length-1] + "' again.";        
   alert(alertText);
  } 
 })();
--- JSFormatted/window_style_underlineSelectedText.js
+++ JSFormatted/window_style_underlineSelectedText.js
@@ -1,100 +1,137 @@
 javascript:(function() {
  /* OPTIONAL - set to 0 or delete to turn off instruciton pop-up, */
- var alertWithInstructions_underlineText = 1;
+ var alertWithInstructions_underlineText = 1;  /* turn instruction alert on or off                 */
+ var noPressKey_underlineText = "SHIFT";       /* button to press when using with other bookmarkel */
 
- /* Define global variables.                    */ 
- var currentUnderlineCount = 1; /* count number of times run to get unique ids */
- var currentStyleOpenTag_underlineText  = "<u>";  /* open tag to style selection  */
- var currentStyleCloseTag_underlineText = "</u>"; /* close tag to style selection */
- var keyCombo_underlineText = "Control+;";        /* key combo to change style */
+ /* Define global variables. */ 
+ var numberOfKeyPresses_underlineText = 3;/* specify how many keypresses are used            */
+ var currentStyleOpenTag_underlineText  = "<u>";  /* open tag to style selection     */
+ var currentStyleCloseTag_underlineText = "</u>"; /* close tag to style selection    */ 
+ var keyCombo_underlineText = "Control+Shift+u"; /* key combo to change style         */ 
  
  /* Declare global variables */
  var range_underlineText, selection_underlineText, selectionParent_underlineText, 
      selectedString_underlineText, parentIDWithSelectedText_underlineText;
  
- /***** MAIN FUNCTION *****/
- function keypressToBookmarklet_underlineText() {
-  /* keyboard combo that enablse text to be style underline */
-  let lastUnderlineKeypress = sessionStorage.getItem("bm_last_underline_keypress");
-  let currentUnderlineKeyPress = event.key;
-
-  /* check if sessionStorage already contains data with above var names */
-  if (lastUnderlineKeypress == null) {
-   sessionStorage.setItem("bm_last_underline_keypress", event.key);
-   lastUnderlineKeypress = sessionStorage.getItem("bm_last_underline_keypress");
+ /******************************* MAIN FUNCTION *******************************/
+ /*****************************************************************************/
+ var keySwitch_underlineText = 0;      
+ var lastThreeKeys_underlineText = []; /* enables last three key presses to be stored */
+ function keypressToBookmarklet_underlineText(key_press) {
+  /* keyboard combo that enablse text to be style underline */       
+  lastThreeKeys_underlineText.push(key_press);
+  
+  /* keep only the last three keys */
+  if (lastThreeKeys_underlineText.length > numberOfKeyPresses_underlineText) {
+   lastThreeKeys_underlineText.shift();
   }
-  /* variable to check key combo */
-  let checkKeyComboUnderlineText = 
-   lastUnderlineKeypress + "+" + currentUnderlineKeyPress;
-
-  /* check if key combo is ctrl + u */
-  if (checkKeyComboUnderlineText == keyCombo_underlineText) {   
-   currentUnderlineCount += 1;         /* increment count for unique id   */
-   getParentElementOfSelection(); /* get parent of selected text     */
-   
-   /* add a delay to ensure two previous blocks run */ 
-   setTimeout(function() { 
-    underlineTheSelectedText(); /* wrap the selected text in <u> tag */
-   }, 100);
+  
+  /* check key combo                                   */
+  if (numberOfKeyPresses_underlineText == 2) {      
+   /* check if key combo is keyCombo_underlineText variable */
+   let checkIfKeyCombo_underlineText = 
+    lastThreeKeys_underlineText[0] + "+" +
+    lastThreeKeys_underlineText[1];
+   if (checkIfKeyCombo_underlineText == keyCombo_underlineText) {   
+    getParentElementOfSelection_underlineText(); /* get parent of selected text     */
+   }
+  } 
+  else if (numberOfKeyPresses_underlineText == 3) {
+   /* check if key combo is keyCombo_underlineText variable */
+   let checkIfKeyCombo_underlineText = 
+    lastThreeKeys_underlineText[0] + "+" +
+    lastThreeKeys_underlineText[1] + "+" +
+    lastThreeKeys_underlineText[2];   
+   if (checkIfKeyCombo_underlineText == keyCombo_underlineText) {
+    getParentElementOfSelection_underlineText(); /* get parent of selected text     */
+   }    
+  }
+  else {
+   /* check if key combo is keyCombo_underlineText variable */
+   let checkIfKeyCombo_underlineText = 
+    lastThreeKeys_underlineText[0];
+   if (checkIfKeyCombo_underlineText == keyCombo_underlineText) {   
+    getParentElementOfSelection_underlineText(); /* get parent of selected text     */
+   }
   }
  }
 
 
  /***** Support Functions: *****/ 
  /* Get parent of selection, give it id and use it to style selected text. */
- const getParentElementOfSelection = () => {
+ const getParentElementOfSelection_underlineText = () => {
    /* get the current selection */
    selection_underlineText = window.getSelection();
    
-   if (selection_underlineText.rangeCount > 0) { /* ensure something is selected            */
-    /* prepare elements to change selection                                  */
-    range_underlineText = selection_underlineText.getRangeAt(0);  /* first object of selection       */
+   if (selection_underlineText.rangeCount > 0) { /* ensure something is selected                 */
+    /* prepare elements to change selection                                                 */
+    range_underlineText = selection_underlineText.getRangeAt(0);  /* first object of selection        */
     selectionParent_underlineText = range_underlineText.commonAncestorContainer; /* node of selection */
-
-    selectedString_underlineText = range_underlineText.toString(); /* store as string      */     
-    /* ensure selection is a text node                                  */
-    if (selectionParent_underlineText.nodeType === 3) { /* if it is a text node        */
-     let curPar = selectionParent_underlineText.parentElement; /* store the parent element  */
-
-     /* give parent a unique id */
-     if (curPar.hasAttribute("id") == true) {
-      let curID = curPar.id;
-      if (curID.indexOf("bm_window_style_underlineSelectedText_") > -1) {       
-       /* don't include prior unique ids */
-       curID = curID.replace(/bm_window_style_underlineSelectedText_[0-9]+/g, "bm_window_style_underlineSelectedText_" + currentUnderlineCount);
-       /* set the parents' id to unique id */
-       curPar.id = curID;
-      } else {
-       /* append unique id to the current id value */
-       curPar.id += " bm_window_style_underlineSelectedText_" + currentUnderlineCount;
-      }
+ 
+    let temp = document.getElementById("bm_window_style");
+    if (temp) {
+     /* don't include prior global style ids */       
+     let tempID = temp.getAttribute("id");
+     if (tempID == "bm_window_style") {
+      temp.removeAttribute("id");
      } else {
-      /* set the parents' id to unique id */
-      curPar.setAttribute("id", "bm_window_style_underlineSelectedText_" + currentUnderlineCount);
-     }       
-
-     /* select by id to get parent element */
-     parentIDWithSelectedText_underlineText = 
-      document.getElementById("bm_window_style_underlineSelectedText_" + currentUnderlineCount);
-    } else {
-     let skip;
+      temp.setAttribute("id", tempID.replace("bm_window_style", ""));
+     }     
     }
-   }
+    
+     selectedString_underlineText = range_underlineText.toString(); /* store as string            */     
+     /* ensure selection is a text node                                                 */
+     if (selectionParent_underlineText.nodeType === 3) { /* if it is a text node             */
+      let curPar = selectionParent_underlineText.parentElement; /* store the parent element  */
+      let checkIfStyleApplied = /* check if the style has already been applied          */
+       currentStyleOpenTag_underlineText.replace(/[<>]/g, "").toUpperCase();
+      
+      if (checkIfStyleApplied == curPar.tagName) { /* remove if already applied          */
+        let grandParentOfSelection = curPar.parentElement; /* get tag nesting selection  */
+        let curInnerHTML = grandParentOfSelection.innerText; /* innerText to replace     */
+        curInnerHTML = /* replace innderText, but remove style tag already applied       */
+         curInnerHTML.replace(/currentStyleOpenTag_underlineText/, "").replace(/currentStyleCloseTag_underlineText/, "");
+        /* update with the styled tag removed */
+        grandParentOfSelection.innerHTML = curInnerHTML;        
+      } else {
+      /* give parent a global style id                    */
+      if (curPar.hasAttribute("id") == true) {
+       let curID = curPar.id;
+       if (curID.indexOf("bm_window_style") > -1) {              
+        /* set the parents' id to global style id         */
+        curPar.id = "bm_window_style";
+       } else {
+        /* append global style id to the current id value */
+        curPar.id += " bm_window_style";
+       }
+      } else {
+       /* set the parents' id to global style id */
+       curPar.setAttribute("id", "bm_window_style");
+      }       
+    
+      /* select by id to get parent element */
+      parentIDWithSelectedText_underlineText = 
+       document.getElementById("bm_window_style");
+      /* apply styling to selection */  
+      underlineTheSelectedText();
+    }
+   } 
+  }
  };
  
- /* Style text using the ID added to parent element in getParentElementOfSelection(). */
+ /* Style text using the ID added to parent element in getParentElementOfSelection_underlineText(). */
  const underlineTheSelectedText = () => {
   /* store the curren innerHTML to update with underline text */
   let curInnerHTML = parentIDWithSelectedText_underlineText.innerHTML;
 
   /* wrap selection in HTML u tag                                          */
-  let underlineStyleSelection = /* use variables defined at top and in main function */
+  let underlineStyleSelection = /* use variables defined at top and in main function    */
    currentStyleOpenTag_underlineText + selectedString_underlineText + currentStyleCloseTag_underlineText;
   
   /* if only one occurence of selected text in parent element */
   if (curInnerHTML.indexOf(selectedString_underlineText) == 
       curInnerHTML.lastIndexOf(selectedString_underlineText)) {
-   /* replace selection with newly styled text */
+   /* replace selection with newly styled text  */
    curInnerHTML = curInnerHTML.replace(selectedString_underlineText, underlineStyleSelection);
    
    /* Update parent HTML with newly styled text */
@@ -119,13 +156,15 @@ javascript:(function() {
    parentIDWithSelectedText_underlineText.innerHTML = 
     insertAtIndex(curInnerHTML, underlineStyleSelection, curIndex, curLengthOfText);
   }
+  selection_underlineText = undefined;
+  lastThreeKeys_underlineText = [];
  };
 
  /* Listen form key combo to underline text. */
  const addKeyDown_underlineText = () => { 
-  document.body.addEventListener("keydown", function() {
-   keypressToBookmarklet_underlineText();  
-  }); 
+  document.body.addEventListener("keydown", function(event) {
+   keypressToBookmarklet_underlineText(event.key);  
+  });   
  }; 
  
  /****************************************************************** 
@@ -137,12 +176,21 @@ javascript:(function() {
  /* Add alert event so that instructions for use are communicated. */
  if (alertWithInstructions_underlineText != undefined && 
      alertWithInstructions_underlineText == 1) {
-  let alertText = "Press '" + keyCombo_underlineText + "' to make selected text underline. \n\n" +
+  let alertText = 
+        "INSTRUCTIONS:     \n"   + 
+        "*************     \n"   +
+        "Press '" + keyCombo_underlineText + "' to make selected text underline.             \n" +
+        "To remove press '" + keyCombo_underlineText + "' some of previous styled text. \n\n" +
         "For best results: \n\n" +
-        " - Avoid selecting text with different sytling. \n"       +
+        " - Avoid selecting text with different sytling.   \n"     +
         " - Avoid selecting a word that is often repeated. \n"     +
-        " - Select text on the same line. \n"                      + 
-        " - Ensure selection contains entire words. \n";   
+        " - Press each key specified one at a time.        \n"     +
+        " - If using with another style bookmarklet, press '"      + noPressKey_underlineText + "'\n" +
+        "   button THREE times with NO text selected       \n"     +
+        "   before styling another with a different style  \n"     +
+        "   and key combo.                                 \n"     +
+        " - If style is not applied, press just '" + 
+        keyCombo_underlineText[keyCombo_underlineText.length-1] + "' again.";        
   alert(alertText);
  } 
 })();
